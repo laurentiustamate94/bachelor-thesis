2fcfa9f55b84243d9ff31e1c988e53f577b31739
laurentiustamate94 <laurentiustamate94@yahoo.com>
2017-06-15 20:50:36 +0300

    Create translator interface

---
+++ BachelorThesis.Abstractions/ITranslatorService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BachelorThesis.Abstractions
+{
+    public interface ITranslatorService
+    {
+        Task<string> TranslateToEnglish(string text);
+
+        Task<string> TranslateToLocale(string text);
+    }
+}
