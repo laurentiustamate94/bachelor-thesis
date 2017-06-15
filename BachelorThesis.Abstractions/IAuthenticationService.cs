5587a60dc25994097669ecc8f896149b434934be
laurentiustamate94 <laurentiustamate94@yahoo.com>
2017-06-15 20:50:20 +0300

    Create authentication interface

---
+++ BachelorThesis.Abstractions/IAuthenticationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BachelorThesis.Abstractions
+{
+    public interface IAuthenticationService
+    {
+        string Token { get; }
+
+        void InitializeService(string subscriptionKey);
+    }
+}
