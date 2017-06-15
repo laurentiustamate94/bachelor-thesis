14e1fc16a59e4fa4a28d746017d66ea41d4d8156
laurentiustamate94 <laurentiustamate94@yahoo.com>
2017-06-15 20:57:59 +0300

    Add helper to obtain container in dialog after deserialization

---
+++ BachelorThesis/Helpers/Container.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+
+namespace BachelorThesis.Helpers
+{
+    public class Container
+    {
+        private Container()
+        {
+
+        }
+
+        public static ILifetimeScope GetContainer()
+        {
+            var config = GlobalConfiguration.Configuration;
+            var resolver = (AutofacWebApiDependencyResolver)config.DependencyResolver;
+
+            return resolver.Container;
+        }
+
+        public static T Resolve<T>()
+        {
+            return GetContainer().Resolve<T>();
+        }
+    }
+}
