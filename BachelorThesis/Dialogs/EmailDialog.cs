2ed9006450f3c327608ed9e00960df08625e38d3
laurentiustamate94 <laurentiustamate94@yahoo.com>
2017-06-20 00:54:43 +0300

    Add Email Dialog

---
+++ BachelorThesis/Dialogs/EmailDialog.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Threading.Tasks;
+using BachelorThesis.Abstractions;
+using BachelorThesis.Helpers;
+using BachelorThesis.Models;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace BachelorThesis.Dialogs
+{
+    [Serializable]
+    public sealed class EmailDialog : IDialog<EmailDialogResult>
+    {
+        private readonly ICollection<string> possibleYes =
+            ConfigurationManager.AppSettings["EmailDialog.PossibleYes"].Split(',');
+
+        private readonly ICollection<string> possibleNo =
+            ConfigurationManager.AppSettings["EmailDialog.PossibleNo"].Split(',');
+
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var loggingService = Container.Resolve<ILoggingService>();
+            var activity = await result as Activity;
+
+            if (string.IsNullOrEmpty(activity?.Text))
+            {
+                context.Wait(MessageReceivedAsync);
+
+                return;
+            }
+
+            loggingService.Log(activity.Id, activity.Text);
+
+            var translatorService = Container.Resolve<ITranslatorService>();
+            var activityText = await translatorService.TranslateToEnglish(activity.Text);
+
+            loggingService.Log(loggingService.GetLogIdByMessageId(activity.Id), LogStep.TextTranslate, activityText);
+
+            if (possibleYes.Contains(activityText.ToLower()))
+            {
+                await PositiveResponse(context, translatorService);
+
+                return;
+            }
+
+            if (possibleNo.Contains(activityText.ToLower()))
+            {
+                await NegativeResponse(context, translatorService);
+
+                return;
+            }
+
+            if (new EmailAddressAttribute().IsValid(activity.Text))
+            {
+                context.Done(new EmailDialogResult()
+                {
+                    EmailAddress = activity.Text,
+                    WasEmailAddressSubmitted = true
+                });
+
+                return;
+            }
+
+            var analysisService = Container.Resolve<IAnalysisService>();
+            var sentiment = await analysisService.GetSentiment(activityText);
+
+            loggingService.Log(loggingService.GetLogIdByMessageId(activity.Id), LogStep.TextAnalysis, sentiment.ToString());
+
+            if (sentiment <= Convert.ToDouble(ConfigurationManager.AppSettings["NegativeThreshold"]))
+            {
+                await NegativeResponse(context, translatorService);
+
+                return;
+            }
+            else if (sentiment >= Convert.ToDouble(ConfigurationManager.AppSettings["PositiveThreshold"]))
+            {
+                await PositiveResponse(context, translatorService);
+
+                return;
+            }
+
+            var wrongEmail = await translatorService
+                .TranslateToLocale(ConfigurationManager.AppSettings["EmailDialog.WrongEmail"]);
+
+            await context.PostAsync(wrongEmail);
+
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private static async Task NegativeResponse(IDialogContext context, ITranslatorService translatorService)
+        {
+            var noAnswer = await translatorService
+                .TranslateToLocale(ConfigurationManager.AppSettings["EmailDialog.NoAnswer"]);
+
+            await context.PostAsync(noAnswer);
+
+            context.Done(new EmailDialogResult()
+            {
+                WasEmailAddressSubmitted = false
+            });
+        }
+
+        private async Task PositiveResponse(IDialogContext context, ITranslatorService translatorService)
+        {
+            var yesAnswer = await translatorService
+                .TranslateToLocale(ConfigurationManager.AppSettings["EmailDialog.YesAnswer"]);
+
+            await context.PostAsync(yesAnswer);
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
---
+++ BachelorThesis/Models/EmailDialogResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BachelorThesis.Models
+{
+    [Serializable]
+    public sealed class EmailDialogResult
+    {
+        public string EmailAddress { get; set; }
+
+        public bool WasEmailAddressSubmitted { get; set; }
+    }
+}
