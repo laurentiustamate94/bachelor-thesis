bea87a214935728e0e84d1491b80430b13ca091e
laurentiustamate94 <laurentiustamate94@yahoo.com>
2017-06-20 00:55:12 +0300

    Add feedback dialog

---
+++ BachelorThesis/Dialogs/FeedbackDialog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using BachelorThesis.Abstractions;
+using BachelorThesis.Helpers;
+using Microsoft.Bot.Builder.FormFlow;
+
+namespace BachelorThesis.Dialogs
+{
+    [Serializable]
+    public sealed class FeedbackDialog
+    {
+        public string Feedback { get; set; }
+
+        public static IForm<FeedbackDialog> BuildForm()
+        {
+            var translatorService = Container.Resolve<ITranslatorService>();
+            var initialGreetingTask = Task.Run(
+                () => translatorService.TranslateToLocale(ConfigurationManager.AppSettings["FeedbackDialog.InitialGreeting"]));
+
+            initialGreetingTask.Wait();
+
+            return new FormBuilder<FeedbackDialog>()
+                .Field(name: nameof(Feedback), prompt: initialGreetingTask.Result)
+                .AddRemainingFields()
+                .Build();
+        }
+    }
+}
