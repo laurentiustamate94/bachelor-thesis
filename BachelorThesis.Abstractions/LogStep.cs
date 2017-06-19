0d8be7213a10bdd9f03b34299c4fd9138a86d788
laurentiustamate94 <laurentiustamate94@yahoo.com>
2017-06-20 00:53:02 +0300

    Add enum for specifying the logstep of the message

---
+++ BachelorThesis.Abstractions/LogStep.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BachelorThesis.Abstractions
+{
+    public enum LogStep
+    {
+        UserInput,
+        TextTranslate,
+        QnAMaker,
+        Luis,
+        TextAnalysis,
+        CustomAnswer
+    }
+}
