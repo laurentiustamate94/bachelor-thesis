e80e42a551d8b44040d263c4d37d1b64adabb2fd
laurentiustamate94 <laurentiustamate94@yahoo.com>
2017-06-20 00:56:41 +0300

    Implement Autofac context

--- BachelorThesis/Global.asax.cs
+++ BachelorThesis/Global.asax.cs
@@ -1,9 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
-using System.Web;
+using System.Reflection;
 using System.Web.Http;
-using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.WebApi;
+using BachelorThesis.Dialogs;
+using BachelorThesis.Services;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.Luis;
+using QnAMakerDialog;
 
 namespace BachelorThesis
 {
@@ -11,7 +19,66 @@ namespace BachelorThesis
     {
         protected void Application_Start()
         {
+            var builder = new ContainerBuilder();
+            var config = GlobalConfiguration.Configuration;
+
+            RegisterDependencies(builder);
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly())
+                .PropertiesAutowired();
+
+            builder.RegisterWebApiFilterProvider(config);
+
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+        private void RegisterDependencies(ContainerBuilder builder)
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory + "bin\\";
+            var types = Directory.EnumerateFiles(path, "*.dll", SearchOption.TopDirectoryOnly)
+                    .Where(filePath => Path.GetFileName(filePath).StartsWith("BachelorThesis"))
+                    .Select(Assembly.LoadFrom)
+                    .ToArray();
+
+            var translatorAuthentication = new AuthenticationService(ConfigurationManager.AppSettings["TranslatorTextApiKey"]);
+            var translatorService = new TranslatorService(translatorAuthentication);
+            builder.RegisterInstance(translatorService)
+                .AsImplementedInterfaces()
+                .AsSelf()
+                .SingleInstance();
+            builder.RegisterInstance(new AnalysisService())
+                .AsImplementedInterfaces()
+                .AsSelf()
+                .SingleInstance();
+            builder.RegisterType<LoggingService>()
+                .AsImplementedInterfaces()
+                .AsSelf()
+                .SingleInstance();
+
+            builder.RegisterInstance(new LuisService(new LuisModelAttribute(
+                    ConfigurationManager.AppSettings["LuisModelId"],
+                    ConfigurationManager.AppSettings["LuisSubscriptionKey"])))
+                .As<ILuisService>();
+
+            builder.RegisterType<FeedbackDialog>()
+                .AsSelf()
+                .PropertiesAutowired();
+            builder.
+                Register(c => new FormDialog<FeedbackDialog>(c.Resolve<FeedbackDialog>(), FeedbackDialog.BuildForm, FormOptions.PromptInStart))
+                .AsSelf();
+
+            builder.RegisterAssemblyTypes(types)
+                .Where(t =>
+                    t.IsAssignableTo<IDialog<object>>() ||
+                    t.IsAssignableTo<IDialog<bool>>() ||
+                    t.IsAssignableTo<LuisDialog<bool>>() ||
+                    t.IsAssignableTo<QnAMakerDialog<object>>())
+                .AsImplementedInterfaces()
+                .AsSelf()
+                .PropertiesAutowired();
         }
     }
 }
