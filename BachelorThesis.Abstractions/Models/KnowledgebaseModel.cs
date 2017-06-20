8ab1379114998cac6ed03cbdc9826955f5cf158e
laurentiustamate94 <laurentiustamate94@yahoo.com>
2017-06-20 16:01:09 +0300

    Add models to use when updating the database

---
+++ BachelorThesis.Abstractions/Models/KnowledgebaseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BachelorThesis.Abstractions.Models
+{
+    public sealed class KnowledgebaseModel
+    {
+        public string Question { get; set; }
+
+        public string Answer { get; set; }
+
+        public string Source { get; set; }
+    }
+}
---
+++ BachelorThesis.Abstractions/Models/TrainingModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BachelorThesis.Abstractions.Models
+{
+    public sealed class TrainingModel
+    {
+        public string UserId { get; set; }
+
+        public string UserQuestion { get; set; }
+
+        public string KbQuestion { get; set; }
+
+        public string KbAnswer { get; set; }
+    }
+}
