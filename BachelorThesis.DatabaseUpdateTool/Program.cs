c93f0ba8d574e0e511aa7fd50765378c4dc9aacc
laurentiustamate94 <laurentiustamate94@yahoo.com>
2017-06-20 16:00:46 +0300

    Add database update tool console project

---
+++ BachelorThesis.DatabaseUpdateTool/Program.cs
@@ -0,0 +1,26 @@
+﻿using Topshelf;
+
+namespace BachelorThesis.DatabaseUpdateTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<UpdateWorker>(s =>
+                {
+                    s.ConstructUsing(name => new UpdateWorker());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Bachelor Thesis chatbot");
+                x.SetDisplayName("BachelorThesis");
+                x.SetServiceName("BachelorThesis");
+            });
+        }
+    }
+}
